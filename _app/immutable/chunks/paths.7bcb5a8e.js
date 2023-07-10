a2597f7e841830fe50984dfdbb8813caca49e107
DoormatIka <trashcanartistlily@gmail.com>
2023-07-10 17:42:06 +0900

    Updates

---
+++ _app/immutable/chunks/paths.7bcb5a8e.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_fgekxd)==null?void 0:s.base)??"/small-scans";var a;const t=((a=globalThis.__sveltekit_fgekxd)==null?void 0:a.assets)??e;export{t as a,e as b};
